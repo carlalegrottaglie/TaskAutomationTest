d88b2bda3870055c22c92838ebcb130a45b4784b
Carla Flavia Legrottaglie <carla.legrottaglie@gmail.com>
2024-02-29 00:03:23 -0300

    SauceLabTests 3 complete test running ok

--- src/main/java/com/solvd/carina/demo/gui/carinademo/android/Frame.java
+++ src/main/java/com/solvd/carina/demo/gui/carinademo/android/Frame.java
@@ -1,11 +1,15 @@
 package com.solvd.carina.demo.gui.carinademo.android;
 
+import com.carina.demo.mobile.gui.pages.common.UIElementsPageBase;
+import com.solvd.carina.demo.gui.carinademo.common.FrameBasePage;
+import com.solvd.carina.demo.gui.carinademo.common.UIElementsBasePage;
+import com.zebrunner.carina.utils.factory.DeviceType;
+import com.zebrunner.carina.utils.mobile.IMobileUtils;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
-import com.zebrunner.carina.webdriver.gui.AbstractPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
-
-public class Frame extends AbstractPage {
+@DeviceType(pageType = DeviceType.Type.ANDROID_PHONE, parentClass = FrameBasePage.class)
+public class Frame extends FrameBasePage implements IMobileUtils {
 
    @FindBy( id = "Navigate up")
     protected ExtendedWebElement navigateUp;
@@ -19,11 +23,44 @@ public class Frame extends AbstractPage {
     @FindBy( id = "com.solvd.carinademoapplication:id/design_menu_item_text")
     protected ExtendedWebElement uielements2;
 
-    protected Frame(WebDriver driver) {
+    @FindBy(className = "android.widget.ImageButton")
+    private ExtendedWebElement leftMenuButton;
+
+    public Frame(WebDriver driver) {
         super(driver);
     }
 
+    @Override
+    public void FrameBasePage(WebDriver driver) {
+        this.driver= driver;
+    }
+
+@Override
+    public void uielementsClick()
+    {
+        uielements.click();
+    }
+
+    @Override
+    public UIElementsPageBase navigateToUIElementsPage() {
+        leftMenuButton.click();
+        uielements.click();
+        return initPage(getDriver(), UIElementsPageBase.class);
+    }
+//
+//    @Override
+//    public UIElementsBasePage getUiElementsBasePage() {
+//        return null;
+//    }
+//
+//    @Override
+//    public void setUiElementsBasePage(UIElementsBasePage uiElementsBasePage) {
+//        this.uiElementsBasePage= uiElementsBasePage;
+//    }
+
     public void navigatorUpClick()
-    { navigateUp.click();
-         }
+    { navigateUp.click();          }
+
+
+
 }

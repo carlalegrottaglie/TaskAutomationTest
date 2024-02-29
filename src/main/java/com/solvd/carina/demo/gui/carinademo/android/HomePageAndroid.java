e6eef4b7d9fb7ef47164d1035f649b7f276df44c
Carla Flavia Legrottaglie <carla.legrottaglie@gmail.com>
2024-02-28 23:36:59 -0300

    SauceLabTests 3 complete test running ok

--- src/main/java/com/solvd/carina/demo/gui/carinademo/android/HomePageAndroid.java
+++ src/main/java/com/solvd/carina/demo/gui/carinademo/android/HomePageAndroid.java
@@ -2,7 +2,6 @@ package com.solvd.carina.demo.gui.carinademo.android;
 
 import com.solvd.carina.demo.gui.carinademo.common.HomePageBase;
 import com.solvd.carina.demo.gui.carinademo.common.LoginPageBase;
-import com.solvd.carina.demo.gui.carinademo.component.ComponentBase;
 import com.zebrunner.carina.utils.factory.DeviceType;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import org.openqa.selenium.WebDriver;
@@ -11,8 +10,7 @@ import org.openqa.selenium.support.FindBy;
 @DeviceType(pageType = DeviceType.Type.ANDROID_PHONE, parentClass = HomePageBase.class)
 public class HomePageAndroid extends HomePageBase {
 
-    @FindBy(id = "next_button")
-    private Component component;
+
    @FindBy(id = "next_button")
    private ExtendedWebElement nextBtn;
     public HomePageAndroid(WebDriver driver) {
--- src/main/java/com/solvd/carina/demo/gui/carinademo/android/UIElementsPage.java
+++ src/main/java/com/solvd/carina/demo/gui/carinademo/android/UIElementsPage.java
@@ -1,12 +1,22 @@
 package com.solvd.carina.demo.gui.carinademo.android;
 
 
+import com.solvd.carina.demo.gui.carinademo.common.UIElementsBasePage;
+import com.solvd.carina.demo.gui.carinademo.common.LoginPageBase;
+import com.solvd.carina.demo.gui.carinademo.common.UIElementsBasePage;
+import com.zebrunner.carina.utils.factory.DeviceType;
+import com.zebrunner.carina.utils.mobile.IMobileUtils;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.gui.AbstractPage;
+import org.apache.xmlbeans.GDuration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 
-public class UIElementsPage extends AbstractPage {
+import java.lang.reflect.Constructor;
+import java.time.Duration;
+
+@DeviceType(pageType = DeviceType.Type.ANDROID_PHONE, parentClass = UIElementsBasePage.class)
+public class UIElementsPage extends UIElementsBasePage implements IMobileUtils {
 
     @FindBy(id = "editText")
     private ExtendedWebElement name;
@@ -22,7 +32,6 @@ public class UIElementsPage extends AbstractPage {
     private ExtendedWebElement radioButtonFemale;
     @FindBy(id = "radioButton5")
     private ExtendedWebElement radioButtonOther;
-    //hacer una lista de elementos de entrada oara varios valores
 
     @FindBy(id = "progressBar")
     private ExtendedWebElement progressBar;
@@ -36,14 +45,108 @@ public class UIElementsPage extends AbstractPage {
     @FindBy(id = " switch1")
     private ExtendedWebElement enabled;
 
-    private Frame contenedor;
+    @FindBy(className = "android.widget.ScrollView")
+    private ExtendedWebElement container;
 
-    protected UIElementsPage(WebDriver driver) {
+    public UIElementsPage(WebDriver driver) {
         super(driver);
-        contenedor = new Frame(getDriver());
-
     }
 
-    public Frame getContenedor()
-    { return contenedor;}
+
+    @Override
+    public void typeText(String text) {
+        name.type(text);
+    }
+
+    @Override
+    public void typeEmail(String email) {
+        this.email.type(email);
+    }
+
+    @Override
+    public String getEmail(){
+        return email.getText();
+    }
+
+    @Override
+    public String getText(){
+        return name.getText();
+    }
+
+    @Override
+    public String getDate(){
+        return date.getText();
+    }
+
+    @Override
+    public void typeDate(String date) {
+        this.date.type(date);
+    }
+
+    @Override
+    public void clickOnMaleRadioButton() {
+        radioButtonMale.click();
+    }
+
+    @Override
+    public void clickOnFemaleRadioButton() {
+        radioButtonFemale.click();
+    }
+
+    @Override
+    public void clickOnOtherRadioButton() {
+        radioButtonFemale.click();
+    }
+
+    @Override
+    public boolean isFemaleRadioButtonSelected(){
+        return radioButtonFemale.isChecked();
+    }
+
+    @Override
+    public boolean isOthersRadioButtonSelected(){
+        return radioButtonFemale.isChecked();
+    }
+
+    @Override
+    public void checkCopy() {
+        copyCheckbox.click();
+    }
+
+    @Override
+    public boolean isCopyChecked(){
+        return copyCheckbox.isChecked();
+    }
+
+    public void swipeToFemaleRadioButton() {
+        swipe(radioButtonFemale, container, 10);
+    }
+    @Override
+    public void  setProgressBar (int i)
+    {
+        progressBar.isVisible();
+
+    }
+    @Override
+    public void  setProgressBar2 (int i)
+    {
+        progressBar2.isVisible();
+        progressBar2.select(i);
+    }
+    @Override
+    public void  setSeekBar (int i)
+    {
+        seekBar.isVisible();
+        seekBar.select(i);
+    }
+    @Override
+    public void  setEnabled ()
+    {
+        enabled.scrollTo();
+
+    }
+    public void swipeToEnabled() {
+        swipe(enabled, container, 5);
+    }
+
 }
